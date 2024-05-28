d79d8061ef5e81fca29c188385655b82f043aa1b
ali-nasiri-80 <alinasiri576@gmail.com>
2024-05-28 07:12:32 +0000

    DesktopMode: Add POST_NOTIFICATIONS permission
    Change-Id: I3438c522462ce1f6c4d859e1d6e40f32712daf17

--- app/src/main/java/com/libremobileos/desktopmode/SettingsActivity.java
+++ app/src/main/java/com/libremobileos/desktopmode/SettingsActivity.java
@@ -2,6 +2,10 @@ package com.libremobileos.desktopmode;
 
 import android.os.Bundle;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import androidx.core.content.ContextCompat;
+import androidx.activity.result.contract.ActivityResultContracts;
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
 
 public class SettingsActivity extends CollapsingToolbarBaseActivity {
@@ -13,6 +17,13 @@ public class SettingsActivity extends CollapsingToolbarBaseActivity {
                     .beginTransaction()
                     .replace(com.android.settingslib.R.id.content_frame, new SettingsFragment())
                     .commit();
+        }
+        checkAndRequestForPermissionNotification();
+    }
+    private void checkAndRequestForPermissionNotification() {
+        if (ContextCompat.checkSelfPermission(SettingsActivity.this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+            }).launch(Manifest.permission.POST_NOTIFICATIONS);
         }
     }
 }
